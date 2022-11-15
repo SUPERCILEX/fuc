d3491d4a677f4435d9d31d72981c284a7321946e
Alex Saveau <saveau.alexandre@gmail.com>
2022-11-14 18:59:24 -0800

    Test that deletion doesn't delete the parent
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- fuc_engine/tests/delete.rs
+++ fuc_engine/tests/delete.rs
@@ -14,25 +14,28 @@ fn one_file() {
     fuc_engine::remove_dir_all(&file).unwrap();
 
     assert!(!file.exists());
+    assert!(dir.as_ref().exists());
 }
 
 #[test]
 fn one_dir() {
     let dir = tempdir().unwrap();
-    let file = dir.path().join("dir");
-    fs::create_dir(&file).unwrap();
-    assert!(file.exists());
+    let root = dir.path().join("dir");
+    fs::create_dir(&root).unwrap();
+    assert!(root.exists());
 
-    fuc_engine::remove_dir_all(&file).unwrap();
+    fuc_engine::remove_dir_all(&root).unwrap();
 
-    assert!(!file.exists());
+    assert!(!root.exists());
+    assert!(dir.as_ref().exists());
 }
 
 #[rstest]
 fn uniform(#[values(1_000, 100_000, 1_000_000)] num_files: usize) {
     let dir = tempdir().unwrap();
+    let root = dir.path().join("root");
     Generator::builder()
-        .root_dir(dir.path().to_path_buf())
+        .root_dir(root.clone())
         .num_files_with_ratio(NumFilesWithRatio::from_num_files(
             NonZeroUsize::new(num_files).unwrap(),
         ))
@@ -40,7 +43,8 @@ fn uniform(#[values(1_000, 100_000, 1_000_000)] num_files: usize) {
         .generate(&mut io::sink())
         .unwrap();
 
-    fuc_engine::remove_dir_all(dir.path()).unwrap();
+    fuc_engine::remove_dir_all(&root).unwrap();
 
-    assert!(!dir.path().exists());
+    assert!(!root.exists());
+    assert!(dir.as_ref().exists());
 }
