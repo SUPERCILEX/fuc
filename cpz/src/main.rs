41b8c7784659c16f983123f89815951f47eb5246
Alex Saveau <saveau.alexandre@gmail.com>
2025-02-14 19:31:18 -0500

    v3.1.0
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- cpz/src/main.rs
+++ cpz/src/main.rs
@@ -276,8 +276,10 @@ fn copy(
             let to = {
                 let is_into_directory = *is_into_directory;
                 let mut to = to;
-                if is_into_directory && let Some(name) = from.file_name() {
-                    to.push(name);
+                if is_into_directory {
+                    if let Some(name) = from.file_name() {
+                        to.push(name);
+                    }
                 }
                 to
             };
