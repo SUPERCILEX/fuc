c3023b199945c3683afed454e68e287f34a4012d
Alex Saveau <saveau.alexandre@gmail.com>
2025-02-14 19:31:18 -0500

    v3.0.1
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- fuc_engine/src/ops/copy.rs
+++ fuc_engine/src/ops/copy.rs
@@ -135,7 +135,7 @@ fn schedule_copies<
 mod compat {
     use std::{
         borrow::Cow,
-        cell::{Cell, LazyCell},
+        cell::Cell,
         env,
         ffi::{CStr, CString},
         fmt::{Debug, Formatter},
@@ -150,6 +150,7 @@ mod compat {
     };
 
     use crossbeam_channel::{Receiver, Sender};
+    use once_cell::sync::Lazy as LazyCell;
     use rustix::{
         fs::{
             AtFlags, CWD, FileType, Mode, OFlags, RawDir, StatxFlags, copy_file_range, mkdirat,
@@ -201,7 +202,7 @@ mod compat {
         fn finish(self) -> Result<(), Error> {
             let Self { scheduling } = self;
 
-            if let Ok((tasks, thread)) = LazyCell::into_inner(scheduling) {
+            if let Ok((tasks, thread)) = LazyCell::into_value(scheduling) {
                 drop(tasks);
                 thread.join().map_err(|_| Error::Join)??;
             }
